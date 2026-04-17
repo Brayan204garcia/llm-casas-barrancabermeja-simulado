f5912738c4de6071277a6d80049fad183919fa3e
Bratorre204 <brayan.garciat@unipaz.edu.co>
2026-04-16 20:41:09 -0500

    Se añaden todos los barrios y se implementa el CORS para todos

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.routers import prediccion
 
 app = FastAPI(title="API LLM - ESTIMADOR DE CASAS")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(prediccion.router, prefix="/casas", tags=["Casas"])
